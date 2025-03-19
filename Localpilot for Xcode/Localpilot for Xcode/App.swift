748ad5c07c745060792ef3aff64a1e17345b048f
Nikita Rodin <n1k1tung@gmail.com>
2025-03-20 00:49:53 +0100

    added empty project

---
+++ Localpilot for Xcode/Localpilot for Xcode/App.swift
@@ -0,0 +1,17 @@
+//
+//  Localpilot_for_XcodeApp.swift
+//  Localpilot for Xcode
+//
+//  Created by Nikita Rodin on 20.03.25.
+//
+
+import SwiftUI
+
+@main
+struct Localpilot_for_XcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
